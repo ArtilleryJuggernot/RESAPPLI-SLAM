7181cea2f2bd3a1d8c2e4d2a298fa3f6ab8c5faf
a.houllier <antoine@houllier.com>
2023-02-01 17:08:04 +0100

    oui

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
