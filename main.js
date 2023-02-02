db607172da1741f9b13afab2cfcac9c11640f37d
a.houllier <antoine@houllier.com>
2023-02-02 09:14:46 +0100

    add icons

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function createWindow () {
     width: 800,
     height: 600,
     autoHideMenuBar: true,
+    icon: __dirname +  'icon.ico',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
