e371ff87aa0e1e65cf3efa5e5a981da86a6cdfbd
Hugo Jacquel <hjacquel@cirigroup.com>
2023-06-22 15:41:14 +0200

    password change & starting actions

--- script/class/login.js
+++ script/class/login.js
@@ -10,13 +10,18 @@ import * as fs_utils from '../backend/fs_utils.js'
  * @param {string} user - Nom d'utilisateur
  * @param {string} hashed_passwd - Mot de passe hashé
  * @param {Config} config - Configuration du login
- * @param {int} Permision - Permission du login (0: admin, 2: sécretaire, 3 : guest)
- * @author Hugo Jacquel
+ * @param {int} Permision - Permission du login (0: admin RES, 1 : admin, 2: sécretaire, 3 : guest)
+ * @param {Date} createConnection - Date de création du compte, par défaut aujourd'hui si non renseigné en paramètre (optionel)
+* @author Hugo Jacquel
  */
 class Login {
 
     /**
      * @author Hugo Jacquel
+
+
+
+
      * @param {string} string 
      * @returns {string} - Hash de la string
      */
@@ -34,6 +39,7 @@ class Login {
 
         // Création d'un compte
         if (creationDate == 'DEFAULT') {
+            console.log("Création du compte")
             this.creationDate = new Date();
         }
         // Connexion à un compte déjà existant
@@ -42,19 +48,27 @@ class Login {
         }
             
         
-        this.lastConnection = new Date();
         if (Permision == 0){
-            this.role = "Administrateur";
+            this.role = "Administrateur RES";
         }
-        else if (Permision == 1){
+
+        else if(Permision == 1){
+            this.role = "Administrateur"
+        }
+
+        else if (Permision == 2){
             this.role = "Secrétaire";
         }
-        else if (Permision == 2){
+        else if (Permision == 3){
             this.role = "Guest";
         }
 
     }
+
+
     checkPassword(password) {
+        console.log(this.hash(password))
+        console.log(this.hashed_passwd)
         return this.hash(password) === this.hashed_passwd;
       }
 
@@ -62,11 +76,19 @@ class Login {
         console.log("SessionID : " + this.hash(this.user + this.hashed_passwd))
         return this.hash(this.user + this.hashed_passwd);
     }
-
-    Disconnect(){
-        sessionStorage.clear();
-        this.lastConnection = new Date();
-        fs_utils.save_login(this)
+    displayCreationDate(){
+        this.creationDate = new Date(this.creationDate)
+        
+        let day = this.creationDate.getDay().toString();
+        if (day.length == 1)
+            day = '0' + day;
+        let month = this.creationDate.getMonth().toString();
+        if (month.length == 1)
+            month = '0' + month;
+            
+        let year = this.creationDate.getFullYear().toString();
+        var dt = day + "/" + month + "/" + year;
+        return dt;
     }
 }
 
